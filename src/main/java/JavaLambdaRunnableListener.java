b850df19a32521507ecc26aad5b752cae994a913
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-09-24 21:58:52 -0300

    add listener

---
+++ src/main/java/JavaLambdaRunnableListener.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JavaLambdaRunnableListener {
+
+    public static void main(String[] args) {
+
+        // JAVA 7
+        JButton jButton = new JButton();
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Olá, mundo!");
+            }
+        });
+
+        JButton jButton2 = new JButton();
+        jButton2.addActionListener(e -> System.out.println("Olá, mundo!"));
+
+        // SAM - Single Abstract Method
+        // Qualquer interface que tenha apenas um método abstrato
+    }
+}
