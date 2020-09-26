08f6b8c466e206c8ccdc2889f4dcd929e3af82c2
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-09-26 13:33:29 -0300

    stream reduce

--- src/main/java/JavaStreamReduce.java
+++ src/main/java/JavaStreamReduce.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.DoubleStream;
 
 public class JavaStreamReduce {
 
@@ -26,5 +27,30 @@ public class JavaStreamReduce {
                 .reduce((n1, n2) -> n1 - n2);
         System.out.println(reduceSubtracao);
 
+        Integer reduceSoma2 = list.stream()
+                .reduce(0, (n1, n2) -> n1 + n2);
+        System.out.println(reduceSoma2);
+
+        Integer reduceMultiplicacao2 = list.stream()
+                .reduce(1, (n1, n2) -> n1 * n2);
+        System.out.println(reduceMultiplicacao2);
+
+        String concatenaceo2 = listSplit.stream()
+                .reduce("", (s1, s2) -> s1.concat(s2));
+        System.out.println(concatenaceo2);
+
+        // reduce do menor valor
+        double menorValor = DoubleStream.of(1.5, 2.9, 6.7)
+                .reduce(Double.POSITIVE_INFINITY, (d1, d2) -> Math.min(d1, d2));
+        System.out.println(menorValor);
+
+        // map + combiner
+        String soma = list.stream()
+                .reduce(
+                    "",
+                    (n1, n2) -> n1.toString().concat(n2.toString()),
+                    (n1, n2) -> n1.concat(n2)
+                );
+        System.out.println("Função de acumulação: " + soma);
     }
 }
