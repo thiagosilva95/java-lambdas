322d6a5404f640f644ffe8eb12a8f64b0b3a9b70
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-09-26 13:21:36 -0300

    stream reduce

--- src/main/java/JavaStreamReduce.java
+++ src/main/java/JavaStreamReduce.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 public class JavaStreamReduce {
 
     public static void main(String[] args) {
-
+        String s = "Imagine aqui um texto";
+        String[] split = s.split(" ");
+        List<String> listSplit = Arrays.asList(split);
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+
         Optional<Integer> reduceSoma = list.stream()
                 .reduce((n1, n2) -> n1 + n2);
         System.out.println(reduceSoma);
@@ -14,6 +17,14 @@ public class JavaStreamReduce {
         Optional<Integer> reduceMultiplicacao = list.stream()
                 .reduce((n1, n2) -> n1 * n2);
         System.out.println(reduceMultiplicacao);
+
+        Optional<String> concatenaceo = listSplit.stream()
+                .reduce(((s1, s2) -> s1.concat(s2)));
+        System.out.println(concatenaceo);
+
+        Optional<Integer> reduceSubtracao = list.stream()
+                .reduce((n1, n2) -> n1 - n2);
+        System.out.println(reduceSubtracao);
 
     }
 }
